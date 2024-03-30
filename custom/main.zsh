c2d780c28e66a54c1354c8aa56eb4b7bd44f971d
Henrik L <helagro@outlook.com>
2024-03-30 11:34:48 +0100

    Add my settings

---
+++ custom/main.zsh
@@ -0,0 +1,18 @@
+ZSH_THEME="robbyrussell"
+
+plugins=(git)
+
+unsetopt inc_append_history
+unsetopt share_history
+
+# Disable zsh-autocompletion on paste
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic
+}
+
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
